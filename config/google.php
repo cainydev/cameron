4081660b2ecad56fbd5bc4def6cd5124ae87344e
cainydev <wajo432@gmail.com>
2026-03-16 21:48:24 +0100

    Derive Google redirect URI from APP_URL by default

--- config/google.php
+++ config/google.php
@@ -2,7 +2,7 @@
 
 return [
     'credentials_path' => storage_path('app/google-credentials.json'),
-    'redirect_uri' => env('GOOGLE_REDIRECT_URI', 'http://localhost:8000/auth/google/callback'),
+    'redirect_uri' => env('GOOGLE_REDIRECT_URI', config('app.url').'/auth/google/callback'),
     'ads_developer_token' => env('GOOGLE_ADS_DEVELOPER_TOKEN'),
     'pagespeed_api_key' => env('GOOGLE_PAGESPEED_API_KEY'),
     'scopes' => [
