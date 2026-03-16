742f7d25fa5624004bb25987fdafeb7738d82dc4
cainydev <wajo432@gmail.com>
2026-03-16 21:52:17 +0100

    Add cameron images to Vite manifest

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/images/cameron.png', 'resources/images/cameron_sm.png'],
             refresh: true,
         }),
         tailwindcss(),
